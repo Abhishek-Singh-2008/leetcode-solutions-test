aa16b6a33b9c57c4870320f2e97f824ad5dba463
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-17 12:27:13 +0530

    feat: add Two Sum solution

--- algorithms/two-sum/solution.py
+++ algorithms/two-sum/solution.py
@@ -1,3 +1,4 @@
+class Solution:    #new test final 2 testing with more time for fall back    def twoSum(self, nums, target):        seen = {}        for i, num in enumerate(nums):            complement = target - num                        if complement in seen:                return [seen[complement], i]            seen[num] = i
 class Solution:
     #new test final 2 testing with more time for fall back
     def twoSum(self, nums, target):
