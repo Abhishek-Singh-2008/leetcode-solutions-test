1c7a9f1e6c8555953b0024a33f6ed24a5d142638
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-17 12:51:22 +0530

    feat: add Palindrome Number solution

--- algorithms/palindrome-number/solution.py
+++ algorithms/palindrome-number/solution.py
@@ -1,5 +1,9 @@
+class Solution:    #final testing here    def isPalindrome(self, x: int) -> bool:        s=str(x)                return s[::-1]==s        
 class Solution:
+    #final testing here
+
     def isPalindrome(self, x: int) -> bool:
+
         s=str(x)
-        return s[::-1]==s
         
+        return s[::-1]==s
