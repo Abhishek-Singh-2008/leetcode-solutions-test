b2d3e106aa542d2132c1c4515ab319b020dccdd1
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-24 12:09:27 +0530

    feat: add Course Schedule II solution

--- algorithms/course-schedule-ii/solution.py
+++ algorithms/course-schedule-ii/solution.py
@@ -1,24 +1,19 @@
-1class Solution:
-2    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
-3        indegree = [0]*numCourses
-4        # outdegree = [0]*numCourses
-5        adj = [[] for _ in range(numCourses)]
-6
-7        op=deque()
-8        res=[]
-9        for i in prerequisites:
-10            adj[i[1]].append(i[0])
-11            indegree[i[0]]+=1
-12            # outdegree[i[1]]+=1
-13        for i in range(numCourses):
-14            if indegree[i]==0:
-15                op.append(i)
-16                
-17        while (len(op)!=0):
-18            done = op.popleft()
-19            res.append(done)
-20            for neighbour in adj[done]:
-21                indegree[neighbour]-=1
-22                if indegree[neighbour]==0:
-23                    op.append(neighbour)
-24        return res
+q= deque()        for i in range(numCourses):            if indegree[i]==0:                q.append(i)        res =[]            node = q.popleft()        while q:            res.append(node)            for neighbour in adj[node]:                if indegree[neighbour]==0:            adj[i[1]].append(i[0])            outdegree[i[1]]+=1            indegree[i[0]]+=1        for i in prerequisites:                    q.append(neighbour)                indegree[neighbour]-=1
+        q= deque()
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        res =[]
+            node = q.popleft()
+        while q:
+            res.append(node)
+            for neighbour in adj[node]:
+                if indegree[neighbour]==0:
+
+            adj[i[1]].append(i[0])
+            outdegree[i[1]]+=1
+            indegree[i[0]]+=1
+        for i in prerequisites:
+                    q.append(neighbour)
+
+                indegree[neighbour]-=1
