b44521ffda52ce79db10bbd8dd506fc72f0024db
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-24 12:16:04 +0530

    feat: add Course Schedule II solution

--- algorithms/course-schedule-ii/solution.py
+++ algorithms/course-schedule-ii/solution.py
@@ -1,19 +1,15 @@
-q= deque()        for i in range(numCourses):            if indegree[i]==0:                q.append(i)        res =[]            node = q.popleft()        while q:            adj[i[1]].append(i[0])            outdegree[i[1]]+=1            indegree[i[0]]+=1        for i in prerequisites:        adj = [[] for _ in range(numCourses)]        if len(adj)<numCourses: return []        outdegree = [0]*numCourses        indegree = [0]*numCourses    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:class Solution:
-        q= deque()
+for i in range(numCourses):            if indegree[i]==0:                q.append(i)        res =[]            node = q.popleft()        while q:        q= deque()            res.append(node)            for neighbour in adj[node]:                indegree[neighbour]-=1                if indegree[neighbour]==0:                    q.append(neighbour)        if len(res)!=numCourses: return []        return res
         for i in range(numCourses):
             if indegree[i]==0:
                 q.append(i)
         res =[]
             node = q.popleft()
         while q:
-
-            adj[i[1]].append(i[0])
-            outdegree[i[1]]+=1
-            indegree[i[0]]+=1
-        for i in prerequisites:
-        adj = [[] for _ in range(numCourses)]
-        if len(adj)<numCourses: return []
-        outdegree = [0]*numCourses
-        indegree = [0]*numCourses
-    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
-class Solution:
+        q= deque()
+            res.append(node)
+            for neighbour in adj[node]:
+                indegree[neighbour]-=1
+                if indegree[neighbour]==0:
+                    q.append(neighbour)
+        if len(res)!=numCourses: return []
+        return res
