949bbdf4b7a302f1b98f09c7bac6047a5c19512f
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-24 12:10:48 +0530

    feat: add Course Schedule II solution

--- algorithms/course-schedule-ii/solution.py
+++ algorithms/course-schedule-ii/solution.py
@@ -1,4 +1,4 @@
-q= deque()        for i in range(numCourses):            if indegree[i]==0:                q.append(i)        res =[]            node = q.popleft()        while q:            res.append(node)            for neighbour in adj[node]:                if indegree[neighbour]==0:            adj[i[1]].append(i[0])            outdegree[i[1]]+=1            indegree[i[0]]+=1        for i in prerequisites:                    q.append(neighbour)                indegree[neighbour]-=1
+q= deque()        for i in range(numCourses):            if indegree[i]==0:                q.append(i)        res =[]            node = q.popleft()        while q:            adj[i[1]].append(i[0])            outdegree[i[1]]+=1            indegree[i[0]]+=1        for i in prerequisites:        adj = [[] for _ in range(numCourses)]        if len(adj)<numCourses: return []        outdegree = [0]*numCourses        indegree = [0]*numCourses    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:class Solution:
         q= deque()
         for i in range(numCourses):
             if indegree[i]==0:
@@ -6,14 +6,14 @@ q= deque()        for i in range(numCourses):            if indegree[i]==0:     
         res =[]
             node = q.popleft()
         while q:
-            res.append(node)
-            for neighbour in adj[node]:
-                if indegree[neighbour]==0:
 
             adj[i[1]].append(i[0])
             outdegree[i[1]]+=1
             indegree[i[0]]+=1
         for i in prerequisites:
-                    q.append(neighbour)
-
-                indegree[neighbour]-=1
+        adj = [[] for _ in range(numCourses)]
+        if len(adj)<numCourses: return []
+        outdegree = [0]*numCourses
+        indegree = [0]*numCourses
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+class Solution:
