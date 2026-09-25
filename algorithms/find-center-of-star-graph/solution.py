6bae8de1c96ac7fdbdb63e19b70c44cdd34f83d5
Abhishek Singh <abhisinghjnv143@gmail.com>
2026-09-26 01:20:48 +0530

    refactor: update Find Center of Star Graph (Python3) solution

--- algorithms/find-center-of-star-graph/solution.py
+++ algorithms/find-center-of-star-graph/solution.py
@@ -1,4 +1,3 @@
-class Solution:    def findCenter(self, edges: list[list[int]]) -> int:        if edges[0][0] in edges[1]:            return edges[0][0]        return edges[0][1]
 class Solution:
     def findCenter(self, edges: list[list[int]]) -> int:
         if edges[0][0] in edges[1]:
